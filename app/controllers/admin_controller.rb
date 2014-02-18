7ab4059e94673fdd7d00fa7a65d5aa5c5903f669
John Skinner <jno.skinner@gmail.com>
2014-02-18 22:58:54 +0300

    Use a keys file.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,12 @@ class AdminController < ApplicationController
 
   def login
     if request.post?
+      #render :text => ENV['ConcernKey'].inspect
+      #return
       if params[:key].blank?
         flash[:notice] = 'No key entered.'
       else
-        if params[:key] == ENV['KEY']
+        if (params[:key] == ENV['Ckey'] or params[:key] == ENV['Bkey'])
           flash[:notice] = 'Key accepted for login.'
           session[:key] = 'hK78tsq$55.2y9'
           redirect_to :controller => 'tools', :action => 'bar'
